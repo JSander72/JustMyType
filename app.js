e9ddbd6ef32b379ef039a19b33eafc7bb22677e2
JSander72 <jamesearlsanders2@outlook.com>
2023-10-20 18:39:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const sentences = [
+  'somebody once told me',
+  'the world was gonna roll me'
+
+]
+
+let sentendcesIndex = 0;
+let letterIndex = 0;
+let sentence = sentences[sentenceIndex];
+let letter = sentence[letterIndex];
+let wrongWords = 0;
+
+function logEverything() {
+  console.log({ sentencesIndex, letterIndex, sentence, letter, wrongWords});
+
+}
